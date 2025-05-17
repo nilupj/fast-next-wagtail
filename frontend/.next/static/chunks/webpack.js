a897850dcc896f649ef3764daa5b84e4102c624f
jhgvkjh hgvhg <42765208-lipu21258@users.noreply.replit.com>
2025-05-17 07:54:38 +0000

    Restored to '3b8a5dbd51ba84a3a8e5a7e087e849b02332cdaa'
    Replit-Restored-To: 3b8a5dbd51ba84a3a8e5a7e087e849b02332cdaa

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "de4893d1352ab0b9"; }
+/******/ 		__webpack_require__.h = function() { return "196121438ca679a5"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,6 +1,11 @@
-
 import '../styles/globals.css';
+import Navbar from '../components/Navbar';
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Navbar />
+      <Component {...pageProps} />
+    </>
+  );
 }
