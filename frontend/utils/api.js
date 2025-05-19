a5315e7b6be3f3e6892fc81d0755f34b1dc2f24d
nirvay behara <nirvaybehara@gmail.com>
2025-05-19 12:28:38 +0530

    Your commit message

--- frontend/utils/api.js
+++ frontend/utils/api.js
@@ -21,7 +21,19 @@ api.interceptors.request.use((config) => {
   }
   return config;
 });
-
+api.interceptors.response.use(
+  response => response, 
+  error => {
+    if (error.response) {
+      console.error('API error:', error.response.data);
+    } else if (error.request) {
+      console.error('No response received:', error.request);
+    } else {
+      console.error('Error setting up request:', error.message);
+    }
+    return Promise.reject(error);
+  }
+);
 // Top stories
 export const fetchTopStories = async () => {
   try {
